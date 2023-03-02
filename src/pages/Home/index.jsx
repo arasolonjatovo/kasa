f95faa752f57c4dd5ccf940d6a1e3e028544ea3c
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-02 22:05:16 +0100

    [ADD] banner component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import Header from '../../components/Header'
+import Banner from '../../components/Banner'
 
 export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Accueil</h1>
+      <Banner />
     </div>
   )
 }
