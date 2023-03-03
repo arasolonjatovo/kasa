ac1102799b460ec4a52e139e37b6ba11311f2cf0
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-03 01:41:32 +0100

    [ADD] style to error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
+
 import Header from '../../components/Header'
+import './index.scss'
 
 export default function Error() {
   return (
     <div>
       <Header />
-      <h1>Error</h1>
+      <div className="error">
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+        <Link to="/" className="error__anchor">
+          Retourner sur la page d’accueil
+        </Link>
+      </div>
     </div>
   )
 }
