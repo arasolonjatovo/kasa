2950fa44129c24ff532fc54d5cf76b38e088db02
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-03 01:23:02 +0100

    [ADD] banner to about's page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header'
+import Banner from '../../components/Banner'
+import imageBanner from '../../assets/bg-montagnes.png'
 
 export default function About() {
   return (
     <div>
       <Header />
-      <h1>A propos</h1>
+      <Banner imageUrl={imageBanner} desc="Image de fond avec des montagnes" />
     </div>
   )
 }
