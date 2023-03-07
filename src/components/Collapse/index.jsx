f44ced4ec427758a47d2b3f24108b0f5a6da2b8f
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-07 18:46:13 +0100

    [ADD] collapse component

---
+++ src/components/Collapse/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+
+import './index.scss'
+
+export default function Collapse({ label, text }) {
+  const [isOpen, setIsOpen] = useState(false)
+  return (
+    <div>
+      {isOpen ? (
+        <div className="collapse">
+          <div className="collapse__title-container">
+            <p className="collapse__tilte">{label}</p>
+            <FontAwesomeIcon
+              icon={faChevronUp}
+              onClick={() => setIsOpen(false)}
+            />
+          </div>
+          <p className="collapse__desc">{text}</p>
+        </div>
+      ) : (
+        <div className="collapse">
+          <div className="collapse__title-container">
+            <p className="collapse__tilte">{label}</p>
+            <FontAwesomeIcon
+              icon={faChevronDown}
+              onClick={() => setIsOpen(true)}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,14 +1,20 @@
 import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import Footer from '../../components/Footer'
+import Collapse from '../../components/Collapse'
 
 import imageBanner from '../../assets/bg-montagnes.png'
+
+import { aboutList } from '../../datas/aboutList'
 
 export default function About() {
   return (
     <div>
       <Header />
       <Banner imageUrl={imageBanner} desc="Image de fond avec des montagnes" />
+      {aboutList.map((item, index) => (
+        <Collapse label={item.label} text={item.text} key={index} />
+      ))}
       <Footer />
     </div>
   )
