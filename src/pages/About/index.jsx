6a07db5acd5370d492f4a02a9f9bfb7705f7d0f7
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-17 00:00:16 +0100

    [FIX] collapses on about's page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -7,14 +7,18 @@ import imageBanner from '../../assets/bg-montagnes.png'
 
 import { aboutList } from '../../datas/aboutList'
 
+import './index.scss'
+
 export default function About() {
   return (
-    <div>
+    <div className="about">
       <Header />
       <Banner imageUrl={imageBanner} desc="Image de fond avec des montagnes" />
-      {aboutList.map((item, index) => (
-        <Collapse label={item.label} text={item.text} key={index} />
-      ))}
+      <div className="about__collapses">
+        {aboutList.map((item, index) => (
+          <Collapse label={item.label} text={item.text} key={index} />
+        ))}
+      </div>
       <Footer />
     </div>
   )
