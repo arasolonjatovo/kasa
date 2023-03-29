fdddfbf965262bef6c435ed949aad3416e29c341
Audrey <72028932+arasolonjatovo@users.noreply.github.com>
2023-03-29 22:27:28 +0200

    [FIX] reused code

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -9,32 +9,29 @@ export default function Collapse({ label, text }) {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <div>
-      {isOpen ? (
-        <div className="collapse">
-          <div className="collapse__title-container">
-            <p className="collapse__tilte">{label}</p>
+      <div className="collapse">
+        <div className="collapse__title-container">
+          <p className="collapse__tilte">{label}</p>
+
+          {isOpen ? (
             <img
               src={chevronUp}
               alt="chevron vers le haut"
               onClick={() => setIsOpen(false)}
             />
-          </div>
-          <div>
-            <div className="collapse__desc">{text}</div>
-          </div>
-        </div>
-      ) : (
-        <div className="collapse">
-          <div className="collapse__title-container">
-            <p className="collapse__tilte">{label}</p>
+          ) : (
             <img
               src={chevronDown}
               alt="chevron vers le base"
               onClick={() => setIsOpen(true)}
             />
-          </div>
+          )}
         </div>
-      )}
+        {isOpen && <div>
+          <div className="collapse__desc">{text}</div>
+        </div>}
+      </div>
+
     </div>
   )
 }
